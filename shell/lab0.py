48df6900d7a31e880b49c0e9b3acaba3ba41c7eb
joeyroe <47461916+joeyroe@users.noreply.github.com>
2021-02-21 21:40:10 -0700

    Add files via upload

--- shell/lab0.py
+++ shell/lab0.py
@@ -1,38 +1,45 @@
 from os import read
 
-next = 0
-limit = 0
+buffer = None
 
-# my_getChar calls read to fill a buffer. It gets one character at a time.
-def getChar():
-    global next
-    global limit
+#readString reads a string to a buffer
+def readString():
 
-    if(next == limit): #buffer is empty
-        next = 0 #resetting the buffer
-        limit = read(0, 1000) #read into the buffer
+    global buffer
+    buffer = read(0, 1000) #reads to the buffer
 
-        if(limit == 0): #it's reached the end of the file
-            return "EOF"
-
-    if(next < len(limit) - 1): #checks if its in bounds
-        c = chr(limit[next])
-        next += 1
-        return c
-
-    else:
+    if(buffer == 0): #Buffer has reached the end of the file
         return "EOF"
 
+    return buffer
 
-# getLine gets the next line from file descriptor 0 as a String
-def getLine():
-    global next
-    global limit
+
+def readLine():
+
+    global buffer
     line = ""
-    char = getChar()
-    while(char != '' and char != "EOF"):  # Check to see if we have a character to append.
-        line += char
-        char = getChar()
-    next = 0  # reset next and limit after we have have finished a line.
-    limit = 0
-    return line #returns empty if EOF
+    i = 0
+
+    if(buffer == None): #if the buffer is empty
+        buffer = readString()
+
+    while len(buffer) > 0:
+
+        line += chr(buffer[i]) #add to the line
+        if ("\n" in line): #encounters a new line
+            buffer = buffer[i + 1:]
+            return line
+        i += 1
+
+    if(i == len(buffer)): #resets the buffer
+        i = 0
+        buffer = readString()
+    return line
+
+#def main():
+
+
+ #   b = readLine()
+  #  print(b)
+
+#   main()
